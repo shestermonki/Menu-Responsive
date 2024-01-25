6cf200525d01541776179f4f8fcd81a15aa33856
shestermonki <faustinoludwin@gmail.com>
2024-01-25 15:13:03 +0100

    Menu Responsive

---
+++ script.js
@@ -0,0 +1,20 @@
+let nav_toggle = document.querySelector('.nav_toggle');
+let nav_toggle_icon = document.querySelector('.nav_toggle ion-icon');
+let nav_menu = document.querySelector('.nav_menu');
+
+nav_toggle.addEventListener('click', () => {
+    nav_menu.classList.toggle('active');
+    nav_toggle_icon.setAttribute('name',
+        nav_menu.classList.contains('active') ? 'close-outline' : 'menu-outline');
+
+    document.querySelector(".nav_menu").addEventListener("click", () => {
+
+        document.querySelector(".dropdown").classList.toggle("mostrar");
+    });
+
+});
+
+
+
+
+
